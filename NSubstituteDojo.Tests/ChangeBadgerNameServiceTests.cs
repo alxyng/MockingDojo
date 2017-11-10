f385ffe6736ea58089a4d5d8127e195aa9f70b06
Alex Young <alex.young@myunidays.com>
2017-11-10 16:22:58 +0000

    finished

--- NSubstituteDojo.Tests/ChangeBadgerNameServiceTests.cs
+++ NSubstituteDojo.Tests/ChangeBadgerNameServiceTests.cs
@@ -66,35 +66,14 @@ namespace NSubstituteDojo.Tests
 		    Assert.That(result.Status, Is.EqualTo(ChangeNameStatus.BadgerNotFound));
 	    }
 
-		[Test] // STUBS
-		public async void UpdatingABadgerWithTheSameNameUsingStubs()
+		[Test]
+		public async void UpdatingABadgerWithTheSameNameDoesNotCallUpdate()
 		{
 			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
 			findBadgerByIdQuery
 				.FindById(_badger.Id)
 				.Returns(_badger);
 
-			var validator = Substitute.For<IBadgerNameValidator>();
-			validator
-				.IsValid(Arg.Any<string>())
-				.Returns(BadgerNameValidatorResult.Success);
-
-			var service = new ChangeBadgerNameService(findBadgerByIdQuery, Substitute.For<IUpdateBadgerNameCommand>(), validator);
-
-			var result = await service.ChangeName(_badger.Id, "Boris");
-
-			Assert.That(result.Status, Is.EqualTo(ChangeNameStatus.Ok));
-			Assert.That(result.UpdatedBadger.Name, Is.EqualTo("Boris"));
-		}
-
-		[Test] // MOCKS
-		public async void UpdatingABadgerWithTheSameNameUsingMocks()
-		{
-			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
-			findBadgerByIdQuery
-				.FindById(Arg.Any<Guid>())
-				.Returns(_badger);
-
 			var updateBadgerNameCommand = Substitute.For<IUpdateBadgerNameCommand>();
 
 			var validator = Substitute.For<IBadgerNameValidator>();
@@ -106,15 +85,14 @@ namespace NSubstituteDojo.Tests
 
 			var result = await service.ChangeName(_badger.Id, "Boris");
 
-			await findBadgerByIdQuery.Received(1).FindById(_badger.Id);
 			await updateBadgerNameCommand.Received(0).Update(Arg.Any<Badger>());
 
 			Assert.That(result.Status, Is.EqualTo(ChangeNameStatus.Ok));
 			Assert.That(result.UpdatedBadger.Name, Is.EqualTo("Boris"));
 		}
 
-		[Test] // STUBS
-		public async void UpdatingABadgerWithAValidNameUsingStubs()
+		[Test]
+		public async void UpdatingABadgerWithAValidNameCallsUpdate()
 		{
 			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
 			findBadgerByIdQuery
@@ -132,39 +110,15 @@ namespace NSubstituteDojo.Tests
 
 			var result = await service.ChangeName(_badger.Id, "Brock");
 
-			Assert.That(result.Status, Is.EqualTo(ChangeNameStatus.Ok));
-			Assert.That(result.UpdatedBadger.Name, Is.EqualTo("Brock"));
-
-		}
-
-		[Test] // MOCKS
-		public async void UpdatingABadgerWithAValidNameUsingMocks()
-		{
-			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
-			findBadgerByIdQuery
-				.FindById(Arg.Any<Guid>())
-				.Returns(_badger);
-
-			var updateBadgerNameCommand = Substitute.For<IUpdateBadgerNameCommand>();
-
-			var validator = Substitute.For<IBadgerNameValidator>();
-			validator
-				.IsValid(Arg.Any<string>())
-				.Returns(BadgerNameValidatorResult.Success);
-
-			var service = new ChangeBadgerNameService(findBadgerByIdQuery, updateBadgerNameCommand, validator);
-
-			var result = await service.ChangeName(_badger.Id, "Brock");
-
-			await findBadgerByIdQuery.Received(1).FindById(_badger.Id);
 			await updateBadgerNameCommand.Received(1).Update(Arg.Is<Badger>(b => b.Id == _badger.Id && b.Name == "Brock"));
 
 			Assert.That(result.Status, Is.EqualTo(ChangeNameStatus.Ok));
 			Assert.That(result.UpdatedBadger.Name, Is.EqualTo("Brock"));
+
 		}
 
-		[Test] // STUBS
-		public void QueryDatabaseErrorUsingStubs()
+		[Test]
+		public void QueryDatabaseError()
 		{
 			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
 			findBadgerByIdQuery
@@ -182,84 +136,6 @@ namespace NSubstituteDojo.Tests
 
 			Assert.That(async () => await service.ChangeName(_badger.Id, "Brock"),
 				Throws.Exception.With.Message.EqualTo("Badgers broke the database"));
-		}
-
-		[Test] // MOCKS
-		public void QueryDatabaseErrorUsingMocks()
-		{
-			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
-			findBadgerByIdQuery
-				.FindById(Arg.Any<Guid>())
-				.Throws(new Exception("Badgers broke the database"));
-
-			var updateBadgerNameCommand = Substitute.For<IUpdateBadgerNameCommand>();
-
-			var validator = Substitute.For<IBadgerNameValidator>();
-			validator
-				.IsValid(Arg.Any<string>())
-				.Returns(BadgerNameValidatorResult.Success);
-
-			var service = new ChangeBadgerNameService(findBadgerByIdQuery, updateBadgerNameCommand, validator);
-
-			Assert.That(async () => await service.ChangeName(_badger.Id, "Brock"),
-				Throws.Exception.With.Message.EqualTo("Badgers broke the database"));
-
-			findBadgerByIdQuery.Received(1).FindById(_badger.Id);
-			updateBadgerNameCommand.Received(0).Update(Arg.Any<Badger>());
-		}
-
-		[Test] // STUBS
-		public async void UpdatingTwiceTheSameNameUsingStubs()
-		{
-			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
-			findBadgerByIdQuery
-				.FindById(Arg.Any<Guid>())
-				.Returns(_badger);
-
-			var updateBadgerNameCommand = Substitute.For<IUpdateBadgerNameCommand>();
-
-			var validator = Substitute.For<IBadgerNameValidator>();
-			validator
-				.IsValid(Arg.Any<string>())
-				.Returns(BadgerNameValidatorResult.Success);
-
-			var service = new ChangeBadgerNameService(findBadgerByIdQuery, updateBadgerNameCommand, validator);
-
-			await service.ChangeName(_badger.Id, "Brock");
-
-			var result = await service.ChangeName(_badger.Id, "Brock");
-
-			Assert.That(result.Status, Is.EqualTo(ChangeNameStatus.Ok));
-			Assert.That(result.UpdatedBadger.Name, Is.EqualTo("Brock"));
-		}
-
-		[Test] // MOCKS
-		public async void UpdatingTwiceTheSameNameUsingMocks()
-		{
-			var findBadgerByIdQuery = Substitute.For<IFindBadgerByIdQuery>();
-			findBadgerByIdQuery
-				.FindById(Arg.Any<Guid>())
-				.Returns(_badger, new Badger(_badger.Id, "Brock"));
-
-			var updateBadgerNameCommand = Substitute.For<IUpdateBadgerNameCommand>();
-
-			var validator = Substitute.For<IBadgerNameValidator>();
-			validator
-				.IsValid(Arg.Any<string>())
-				.Returns(BadgerNameValidatorResult.Success);
-
-			var service = new ChangeBadgerNameService(findBadgerByIdQuery, updateBadgerNameCommand, validator);
-
-			await service.ChangeName(_badger.Id, "Brock");
-
-			updateBadgerNameCommand.ClearReceivedCalls();
-
-			var result = await service.ChangeName(_badger.Id, "Brock");
-
-			await updateBadgerNameCommand.Received(0).Update(Arg.Any<Badger>());
-
-			Assert.That(result.Status, Is.EqualTo(ChangeNameStatus.Ok));
-			Assert.That(result.UpdatedBadger.Name, Is.EqualTo("Brock"));
 		}
 	}
 }
